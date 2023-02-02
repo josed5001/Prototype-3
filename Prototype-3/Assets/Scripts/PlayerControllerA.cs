e782d6867f5c8ff78144476c35f88c3791b4b31b
josed5001 <joderas@s.sfusd.edu>
2023-02-02 12:41:05 -0800

    Player jump (second)
    player jump by space

--- Prototype-3/Assets/Scripts/PlayerControllerA.cs
+++ Prototype-3/Assets/Scripts/PlayerControllerA.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class PlayerControllerA : MonoBehaviour
 {
     private Rigidbody playerRB;
+    public float jumpForce = 10;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
-        playerRB.AddForce(Vector3.up * 1000);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
