c9bf47ae5bb64074067bda4d646da8546a1e773a
josed5001 <joderas@s.sfusd.edu>
2023-02-03 12:04:53 -0800

    New Code for Spamming the Keyboard

--- Prototype-3/Assets/Scripts/PlayerControllerA.cs
+++ Prototype-3/Assets/Scripts/PlayerControllerA.cs
@@ -6,19 +6,30 @@ public class PlayerControllerA : MonoBehaviour
 {
     private Rigidbody playerRB;
     public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
     }
+
+    private void  OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+
 }
+
