b120348ab81750dce4aae0dfb75287f12c4013fe
josed5001 <joderas@s.sfusd.edu>
2023-02-14 10:57:52 -0800

    Challenge 3
    completed

--- Prototype-3/Assets/Scripts/SpawnManagerA.cs
+++ Prototype-3/Assets/Scripts/SpawnManagerA.cs
@@ -6,8 +6,8 @@ public class SpawnManagerA : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
-    private float startDelay = 2;
-    private float repeatRate = 2;
+    private float startDelay = 1.51f;
+    private float repeatRate = 1.51f;
     private PlayerControllerA plCtScript;
     // Start is called before the first frame update
     void Start()
